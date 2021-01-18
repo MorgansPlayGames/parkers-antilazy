7830d58df2cda07f6d9b3f8c4027869192cb3ee2
Parker Morgan <jpmaster13@msn.com>
2021-01-18 11:23:17 -0700

    base server

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger("dev"));
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true }).then(() => {
+  app.listen(PORT, () => console.log('Now Listening!'));
+});
