3eefe8034122e2f409aa968a4e46fb658859d3dd
Parker Morgan <jpmaster13@msn.com>
2021-01-18 11:38:35 -0700

    routes routes root fixed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', function(req, res) {
-    res.sendFile("index.html");
+    res.sendFile("./index.html", { root: "./public" });
   });
 
 router.get('/exercise', function(req, res) {
-    res.sendFile("exercise.html");
+    res.sendFile("./exercise.html", { root: "./public" });
   });
 
 router.get('/stats', function(req, res) {
-    res.sendFile("stats.html");
+    res.sendFile("./stats.html", { root: "./public" });
   });
 
   module.exports = router;
